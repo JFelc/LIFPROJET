43be2b5eb24167266f3e3fe2296dc68647b8ab28
JFelc <Julien>
2020-03-29 11:40:19 +0200

    Update Map colors

--- vue/Map.php
+++ vue/Map.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
 	<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
-
+	<script type="text/javascript" src="leaflet-color-markers-master/js/leaflet-color-markers.js"> </script>
 	<!-- Place your stylesheet here-->
 	<link href="css/Map.css" rel="stylesheet" type="text/css">
 </head>
@@ -53,16 +53,14 @@
 				var mymap = L.map('mapid').setView([45.75, 4.85], 13);
 				L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoianVsaWVuZiIsImEiOiJjazZ4dThjajEwN3owM2xta3p5NWN2eWc4In0.xL40ESstdyAMz6gjlVQ2fw', {
 					attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-					maxZoom: 30,
-					minZoom: 12,
 					id: 'mapbox/streets-v11',
 					accessToken: 'pk.eyJ1IjoianVsaWVuZiIsImEiOiJjazZ4dThjajEwN3owM2xta3p5NWN2eWc4In0.xL40ESstdyAMz6gjlVQ2fw'
 
 				}).addTo(mymap);
 
-				//var marker = L.marker([45.75, 4.85]).addTo(mymap);
+				var marker = L.marker([45.75, 4.85]).addTo(mymap);
 
-				var greenIcon = new L.Icon({
+				/*var greenIcon = new L.Icon({
 					iconUrl: 'leaflet-color-markers-master/img/marker-icon-2x-green.png',
 					shadowUrl: 'leaflet-color-markers-master/img/marker-shadow.png',
 					iconSize: [25, 41],
@@ -70,11 +68,37 @@
 					popupAnchor: [1, -34],
 					shadowSize: [41, 41]
 				});
-
-				L.marker([47.75, 4.85], {
+				*/
+				L.marker([45.75, 4.85], {
 					icon: greenIcon
 				}).addTo(mymap);
 
+				/*	function onEachFeature(feature, layer) {
+					// does this feature have a property named popupContent?
+					if (feature.properties && feature.properties.popupContent) {
+						layer.bindPopup(feature.properties.popupContent);
+					}
+				}
+
+
+				var geojsonFeature = {
+					"type": "Feature",
+					"properties": {
+						"name": "Centre de Lyon",
+						"amenity": "Lyon",
+						"popupContent": "Bienvenue au centre de Lyon ! "
+					},
+					"geometry": {
+						"type": "Point",
+						"coordinates": [45.75, 4.85]
+					}
+
+				};
+
+				L.geoJSON(geojsonFeature, {
+					onEachFeature: onEachFeature
+				}).addTo(mymap);
+*/
 				function DPE_indicator(x, y, indice, map) {
 
 					var color = getColor(indice);
